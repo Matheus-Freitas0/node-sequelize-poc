68bd9d81b1a3de65233abe05a85f12718b66989f
MatheusFreitas <moncaomatheus@gmail.com>
2024-10-23 16:30:58 -0300

    update user controller

--- userController.js
+++ userController.js
@@ -4,10 +4,7 @@ class UserController {
     async getAllUsers(req, res) {
         try {
             const users = await User.findAll();
-            res.render('users/index', {
-                title: 'Usuários',
-                users,
-            });
+            res.status(200).json(users); // Retorna a lista de usuários como JSON
         } catch (err) {
             console.error('Erro ao obter usuários:', err);
             res.status(500).json({ error: err.message });
@@ -17,8 +14,8 @@ class UserController {
     async createUser(req, res) {
         const { name, email } = req.body;
         try {
-            await User.create({ name, email });
-            res.redirect('/users');
+            const newUser = await User.create({ name, email });
+            res.status(201).json(newUser); // Retorna o usuário criado como JSON
         } catch (err) {
             console.error('Erro ao criar usuário:', err);
             res.status(500).json({ error: err.message });
@@ -33,7 +30,7 @@ class UserController {
                 return res.status(404).json({ error: 'Usuário não encontrado' });
             }
             await user.destroy();
-            res.redirect('/users');
+            res.status(204).send(); // Retorna 204 No Content após a exclusão
         } catch (error) {
             console.error('Erro ao excluir usuário:', error);
             res.status(500).json({ error: error.message });
@@ -50,13 +47,12 @@ class UserController {
                 return res.status(404).json({ error: 'Usuário não encontrado' });
             }
             await user.update({ name, email });
-            res.redirect('/users');
+            res.status(200).json(user); // Retorna o usuário atualizado como JSON
         } catch (error) {
             console.error('Erro ao editar usuário:', error);
             res.status(500).json({ error: error.message });
         }
     }
-
 }
 
 module.exports = new UserController();
